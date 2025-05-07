09f8137542cf590fcf0a23c53e8dde7132683bbb
LuanGrod <luan14rodrigues17@gmail.com>
2025-05-07 15:39:24 -0300

    mudança do bg color do pwa

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'Estudos para residentes de medicina.',
     start_url: '/',
     display: 'standalone',
-    background_color: '#ffffff',
+    background_color: '#000000',
     theme_color: '#000000',
     icons: [
       {
