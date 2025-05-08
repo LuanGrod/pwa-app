f4afc7b2ac71789166759ca8da37a07d2c5caf2a
LuanGrod <luan14rodrigues17@gmail.com>
2025-05-08 16:52:04 -0300

    correcao da page hot topics - lista

--- src/app/(routes)/hot-topics/lista/page.tsx
+++ src/app/(routes)/hot-topics/lista/page.tsx
@@ -4,8 +4,8 @@ type Props = {};
 
 export default function page({}: Props) {
   return (
-    <ReturnTitleSearchStructure title="Hot Topics">
+    // <ReturnTitleSearchStructure title="Hot Topics">
       <h1>Hot Topics</h1>
-    </ReturnTitleSearchStructure>
+    // </ReturnTitleSearchStructure>
   );
 }
