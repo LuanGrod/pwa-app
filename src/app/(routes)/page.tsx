76bff39f782b561ebfc7312d767df13883dd16c7
LuanGrod <luan14rodrigues17@gmail.com>
2025-05-26 17:02:50 -0300

    correcoes cores e imagens da home

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
         <h1 className={styles.title}>MedRQE</h1>
         <IconFrameContainer
           links={[
-            { href: "/hot-topics", image: "/project/assets/HotTopics2.webp", label: "Hot topics" },
-            { href: "/mapas-mentais", image: "/project/assets/HotTopics2.webp", label: "Mapas mentais" },
-            { href: "/flashcards", image: "/project/assets/HotTopics2.webp", label: "Flashcards" },
-            { href: "/questoes", image: "/project/assets/HotTopics2.webp", label: "Questões" },
-            { href: "/extensivo", image: "/project/assets/HotTopics2.webp", label: "Extensivo" },
-            { href: "/simulado", image: "/project/assets/HotTopics2.webp", label: "Simulado" },
+            { href: "/hot-topics", image: "/project/assets/HotTopics.svg", label: "Hot topics" },
+            { href: "/mapas-mentais", image: "/project/assets/MapasMentais.svg", label: "Mapas mentais" },
+            { href: "/flashcards", image: "/project/assets/Flashcards.svg", label: "Flashcards" },
+            { href: "/questoes", image: "/project/assets/Questoes.svg", label: "Questões" },
+            { href: "/extensivo", image: "/project/assets/Extensivo.svg", label: "Extensivo" },
+            { href: "/simulado", image: "/project/assets/Simulado.svg", label: "Simulado" },
           ]}
         />
         <div></div>
