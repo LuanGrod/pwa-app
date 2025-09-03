c4f7eadf49c25bc14e51099b53d3cb2012050b99
LuanGrod <luan14rodrigues17@gmail.com>
2025-09-03 16:46:35 -0300

    correcao login

--- src/app/(routes)/sair/page.tsx
+++ src/app/(routes)/sair/page.tsx
@@ -2,19 +2,23 @@
 
 import { useEstudante } from "@/store/EstudanteStore";
 import Loading2 from "@global/component/overlay/popup/dialog/Loading2";
+import Cookie from "@global/cookie/Cookie";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { startTransition, useEffect } from "react";
 
 type Props = {};
 
 export default function page({ }: Props) {
   const router = useRouter();
+  const cookie = new Cookie();
   const { clearEstudante } = useEstudante()
 
   useEffect(() => {
     async function handleLogout() {
-      document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      cookie.removeCookie("token");
+      cookie.removeCookie("id");
+      // document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      // document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       clearEstudante();
       router.refresh();
     }
